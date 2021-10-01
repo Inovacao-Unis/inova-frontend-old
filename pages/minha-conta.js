dbdeb1d4155c616768a410c9e062fb8a8cc37ee5
patrickpereirak <patrickpereirak@gmail.com>
2021-10-01 18:35:37 -0300

    add increments

--- pages/minha-conta.js
+++ pages/minha-conta.js
@@ -1,11 +1,14 @@
+/* eslint-disable no-underscore-dangle */
 import { useState, useEffect } from 'react';
 import api from '@services/api';
 import Layout from '@components/Layout';
+import { useAuth } from '@contexts/AuthContext';
 import withAuth from '@components/withAuth';
 import Link from 'next/link';
 import { Container, Heading, Flex, Text, Button } from '@chakra-ui/react';
 
 const minhaConta = () => {
+  const { user, leader } = useAuth();
   const [activities, setActivities] = useState([]);
 
   useEffect(() => {
@@ -30,41 +33,58 @@ const minhaConta = () => {
         <Flex justify="space-between" wrap="wrap">
           {activities && activities.length > 0 ? (
             activities.map((activity) => (
-              // eslint-disable-next-line no-underscore-dangle
-              <Link key={activity.team._id} href={`/trilha/${activity._id}`}>
-                <Flex
-                  direction="column"
-                  justify="center"
-                  align="center"
-                  textAlign="center"
-                  borderRadius="10px"
-                  my="40px"
-                  h="300px"
-                  w="250px"
-                  mx="auto"
-                  bgColor="white"
-                  color="black"
-                  cursor="pointer"
-                >
-                  <Flex direction="column" p="40px">
-                    <Text
-                      fontSize="1.4rem"
-                      lineHeight="20px"
-                      fontWeight="700"
-                      mb="16px"
-                    >
-                      {activity.title}
-                    </Text>
-                    <Button
-                      bgColor="highlight"
-                      color="white"
-                      _hover={{ bg: 'highlight' }}
-                    >
-                      Acessar
-                    </Button>
-                  </Flex>
+              <Flex
+                key={activity.team._id}
+                direction="column"
+                justify="center"
+                align="center"
+                textAlign="center"
+                borderRadius="10px"
+                my="40px"
+                h="300px"
+                w="250px"
+                mx="auto"
+                bgColor="white"
+                color="black"
+              >
+                <Flex direction="column" p="40px">
+                  <Text
+                    fontSize="1.4rem"
+                    lineHeight="20px"
+                    fontWeight="700"
+                    mb="16px"
+                  >
+                    {activity.title}
+                  </Text>
+                  {activity &&
+                  user &&
+                  activity.team.users.includes(user.uid) ? (
+                    <Link href={`/trilha/${activity._id}`}>
+                      <Button
+                        bgColor="highlight"
+                        color="white"
+                        _hover={{ bg: 'highlight' }}
+                        mb="5px"
+                      >
+                        Acessar
+                      </Button>
+                    </Link>
+                  ) : null}
+                  {activity && leader && activity.leaderId === leader ? (
+                    <Link href="#">
+                      <Button
+                        border="2px"
+                        borderColor="highlight"
+                        bg="white"
+                        color="highlight"
+                        _hover={{ bg: 'white' }}
+                      >
+                        Gerenciar
+                      </Button>
+                    </Link>
+                  ) : null}
                 </Flex>
-              </Link>
+              </Flex>
             ))
           ) : (
             <Text>Nenhuma jornada</Text>
---
+++ pages/t/[trailId].js
@@ -0,0 +1,103 @@
+/* eslint-disable consistent-return */
+/* eslint-disable no-underscore-dangle */
+/* eslint-disable array-callback-return */
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/router';
+import {
+  Container,
+  Heading,
+  Text,
+  RadioGroup,
+  Flex,
+  Stack,
+  Radio,
+} from '@chakra-ui/react';
+import Layout from '@components/Layout';
+import withAuth from '@components/withAuth';
+import api from '@services/api';
+
+const TrilhaPage = () => {
+  const Router = useRouter();
+  const { trailId } = Router.query;
+  const [activity, setActivity] = useState(null);
+  const [leader, setLeader] = useState({});
+  const [challenge, setChallenge] = useState(null);
+  const [saude, setSaude] = useState(false);
+  const [gestao, setGestao] = useState(false);
+  const [engenharia, setEngenharia] = useState(false);
+
+  useEffect(() => {
+    const getData = async () => {
+      await api.get(`activity/${trailId}`).then((res) => setActivity(res.data));
+    };
+
+    getData();
+  }, [trailId]);
+
+  useEffect(() => {
+    if (
+      activity?.challenges.some(
+        (e) => e.categoryId === '614389a34db7167c3368f753',
+      )
+    ) {
+      setSaude(true);
+    }
+
+    if (
+      activity?.challenges.some(
+        (e) => e.categoryId === '61438a07f87adb7c88785a41',
+      )
+    ) {
+      setGestao(true);
+    }
+
+    if (
+      activity?.challenges.some(
+        (e) => e.categoryId === '61438a13f87adb7c88785a44',
+      )
+    ) {
+      setEngenharia(true);
+    }
+  }, [activity, setSaude, setGestao, setEngenharia]);
+
+  return (
+    <Layout profile>
+      <Container maxW="container.xl" zIndex="800" pb="100px">
+        <Flex direction="column">
+          <Heading>Participar da trilha</Heading>
+          <p>Líder: {activity?.leader}</p>
+        </Flex>
+        <Flex bg="white" p="30px">
+          <RadioGroup color="black" onChange={setChallenge} value={challenge}>
+            {saude && <Text>Saúde</Text>}
+            <Stack direction="row">
+              {activity?.challenges.map((item) => {
+                if (item.categoryId === '614389a34db7167c3368f753') {
+                  return <Radio value={item._id}>{item.title}</Radio>;
+                }
+              })}
+            </Stack>
+            {gestao && <Text>Gestão</Text>}
+            <Stack direction="row">
+              {activity?.challenges.map((item) => {
+                if (item.categoryId === '61438a07f87adb7c88785a41') {
+                  return <Radio value={item._id}>{item.title}</Radio>;
+                }
+              })}
+            </Stack>
+            {engenharia && <Text>Engenharia</Text>}
+            <Stack direction="row">
+              {activity?.challenges.map((item) => {
+                if (item.categoryId === '61438a13f87adb7c88785a44') {
+                  return <Radio value={item._id}>{item.title}</Radio>;
+                }
+              })}
+            </Stack>
+          </RadioGroup>
+        </Flex>
+      </Container>
+    </Layout>
+  );
+};
+
+export default withAuth(TrilhaPage);
