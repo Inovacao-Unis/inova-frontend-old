fdf7a6e69776157cc17c23d8172c8af832d90193
patrickpereirak <patrickpereirak@gmail.com>
2021-11-09 15:14:38 -0300

    check stage

--- pages/trilha/[trailId]/[stage]/index.js
+++ pages/trilha/[trailId]/[stage]/index.js
@@ -1,5 +1,5 @@
 /* eslint-disable react/no-danger */
-import { useState, useEffect } from 'react';
+import { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { Container, Box, Heading } from '@chakra-ui/react';
 import Link from 'next/link';
